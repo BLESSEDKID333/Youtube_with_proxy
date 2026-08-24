12293344b3415342b7f3073d7940aa3920d0eda1
BLESSEDKID333 <anzartolstiy333@gmail.com>
2026-08-24 22:22:32 +0300

    Upload YouTube/Controllers/TrendingViewController.h

---
+++ YouTube/Controllers/TrendingViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrendingViewController.h
+//  YouTube
+//
+//  Trending / Popular videos screen
+//
+
+#import <UIKit/UIKit.h>
+#import "YouTubeAPIManager.h"
+
+@interface TrendingViewController : UITableViewController <YouTubeAPIManagerDelegate>
+
+@end
